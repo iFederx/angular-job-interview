efc1728377e76d183efae398ece33840a49a7ead
iFederx <federico.brignone@gmail.com>
2025-01-14 02:16:46 +0100

    update

--- README.md
+++ README.md
@@ -10,16 +10,20 @@ Realizza un componente che sia in grado di disegnare una lista di persone (hint:
 
 ```typescript
 const people = [
-  { name: 'Mario', surname: 'Rossi' },
-  { name: 'Giuseppe', surname: 'Neri' },
-  { name: 'Luca', surname: 'Marrone' },
-  { name: 'Laura', surname: 'Bianchi' }
+  { name: 'Mario', surname: 'Rossi', id: '1' },
+  { name: 'Paolo', surname: 'Bianchi', id: '2' },
+  { name: 'Davide', surname: 'Verdi', id: '3' },
+  { name: 'Luca', surname: 'Gialli', id: '4' },
+  { name: 'Giuseppe', surname: 'Neri', id: '5' },
+  { name: 'Luca', surname: 'Marrone', id: '6' },
+  { name: 'Laura', surname: 'Bianchi', id: '7' }
 ];
 ```
 
 ### 1.2
 
-Il componente deve essere in grado di ricevere in input un array di persone. Se l'array passato è vuoto, il componente deve mostrare un messaggio che avvisi che la lista è vuota.
+Il componente deve essere in grado di ricevere in input un array di persone.\
+Se l'array passato è vuoto, il componente deve mostrare un messaggio che avvisi che la lista è vuota.
 
 ### 1.3
 
@@ -29,3 +33,7 @@ Il nome e il cognome della persona devono essere validati: non devono essere vuo
 ### 1.4
 
 Aggiungi, per ogni persona, un bottone **Elimina** che permetta di eliminare la persona dalla lista.
+
+### 1.5
+
+Aggiungi in fondo alla lista un bottone **Fetch** che faccia partire una chiamata verso l'endpoint `https://jsonplaceholder.typicode.com/users` con il metodo GET. La risposta deve essere una lista di persone che devono essere aggiunte alla lista esistente.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 
 // #region Interfaces (1)
 
 export interface Person {
-  // #region Properties (2)
+  // #region Properties (3)
 
+  id: string;
   name: string;
   surname: string;
 
-  // #endregion Properties (2)
+  // #endregion Properties (3)
 }
 
 // #endregion Interfaces (1)
@@ -17,19 +20,26 @@ export interface Person {
 // #region Classes (1)
 
 @Component( {
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    MatIconModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 } )
 export class AppComponent {
   // #region Properties (2)
 
   public people: Person[] = [
-    { name: 'Mario', surname: 'Rossi' },
-    { name: 'Giuseppe', surname: 'Neri' },
-    { name: 'Luca', surname: 'Marrone' },
-    { name: 'Laura', surname: 'Bianchi' }
+    { name: 'Mario', surname: 'Rossi', id: '1' },
+    { name: 'Paolo', surname: 'Bianchi', id: '2' },
+    { name: 'Davide', surname: 'Verdi', id: '3' },
+    { name: 'Luca', surname: 'Gialli', id: '4' },
+    { name: 'Giuseppe', surname: 'Neri', id: '5' },
+    { name: 'Luca', surname: 'Marrone', id: '6' },
+    { name: 'Laura', surname: 'Bianchi', id: '7' }
   ]
   public title = 'job-interview-ex1';
 
